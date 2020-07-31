f8f9d96ed102bc1d84cd174df2b05ff49ff532d5
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-07-31 11:34:48 -0300

    style: add marginTop to button

--- src/pages/checkbox/index.tsx
+++ src/pages/checkbox/index.tsx
@@ -2,10 +2,10 @@ import React, { useRef, useCallback } from 'react';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 
+import { Checkbox } from '../../components/Form';
 import SubmitButton from '../../components/SubmitButton';
 
 import { Container, Logo } from './styles';
-import { Checkbox } from '../../components/Form';
 
 const CheckboxPage: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
@@ -38,6 +38,7 @@ const CheckboxPage: React.FC = () => {
         <SubmitButton
           title="Send"
           onPress={() => formRef.current?.submitForm()}
+          style={{ marginTop: 8 }}
         />
       </Form>
     </Container>
