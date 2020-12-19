48a11e399058a555c6b69d2ee171cbec7928286c
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-12-19 15:11:58 -0300

    chore: Setup Absolute paths

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    ["module-resolver", {
+      alias: {
+        "@components": "./src/components"
+      }
+    }]
+  ]
 };
